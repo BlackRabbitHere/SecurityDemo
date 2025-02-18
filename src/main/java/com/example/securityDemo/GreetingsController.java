0da42edbb9a6a034d8fb033ed30b1133a3d6b3ef
Kshitij <kshitijkumar514@gmail.com>
2025-02-18 19:31:44 +0530

    "Initialized the Role Based security in the System

--- src/main/java/com/example/securityDemo/GreetingsController.java
+++ src/main/java/com/example/securityDemo/GreetingsController.java
@@ -1,6 +1,7 @@
 package com.example.securityDemo;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,18 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello ";
+    }
+
+    @PreAuthorize("hasRole('USER')") // use to set authorize that a person with specific roles in accessing the Website
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "Hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello, Admin";
     }
 
 
